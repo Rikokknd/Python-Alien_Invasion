31e8855ae3309203027d34d571c265d38c012018
Rikokknd <rikodnua@gmail.com>
2020-03-18 23:43:13 +0300

    Learning Python up to page 246

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """Every option of this game"""
+
+    def __init__(self):
+        # Display settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # ship settings
+        self.ship_speed_factor = 4.5
